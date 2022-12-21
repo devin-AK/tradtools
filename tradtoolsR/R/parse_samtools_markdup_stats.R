1e39048d555fa8b9cf8ea6c73f60e70c0d0a0639
devin-AK <devin.king.bio@gmail.com>
2022-12-21 14:06:46 -0500

    Update parse_samtools_markdup_stats.R

--- tradtoolsR/R/parse_samtools_markdup_stats.R
+++ tradtoolsR/R/parse_samtools_markdup_stats.R
@@ -14,10 +14,19 @@
       }
     })
     x <- do.call(rbind,x)
-    Line1 <- c("",x[,1])
+    x <- as.data.frame(x,row.names=x[,1])
+    duplicate_pair_optical <- as.numeric(x['DUPLICATE PAIR OPTICAL',2])
+    duplicate_pair_other   <- as.numeric(x['DUPLICATE PAIR',2]) - duplicate_pair_optical
+    unique_pair            <- as.numeric(x['PAIRED',2]) - (duplicate_pair_optical+duplicate_pair_other)
+    unpaired_excluded      <- as.numeric(x['READ',2]) - (unique_pair+duplicate_pair_optical+duplicate_pair_other)
+    Line1 <- c('','unique_pair','unpaired_excluded','duplicate_pair_optical','duplicate_pair_other')
     Line1 <- paste(Line1,collapse=',')
-    Line2 <- c(ID,as.numeric(x[,2]))
+    Line2 <- c(ID, unique_pair,  unpaired_excluded,  duplicate_pair_optical,  duplicate_pair_other)
     Line2 <- paste(Line2,collapse=',')
+    #Line1 <- c("",x[,1])
+    #Line1 <- paste(Line1,collapse=',')
+    #Line2 <- c(ID,as.numeric(x[,2]))
+    #Line2 <- paste(Line2,collapse=',')
     writeLines(c(Line1,Line2),con=output_file)
   }  
   
