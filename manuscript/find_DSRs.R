ab44444c6bfed342ea7038fb548c9de4deb316a3
devin-AK <devin.king.bio@gmail.com>
2023-02-06 22:53:46 -0500

    Update find_DSRs.R

--- manuscript/find_DSRs.R
+++ manuscript/find_DSRs.R
@@ -74,8 +74,8 @@
                              rowRanges=bins,
                              colData=coldata)
   # STOPPING POINT
-  # # saveRDS(se,'se_020423.RDS')
-  # # se <- readRDS('se_020423.RDS')
+  # # saveRDS(se,'se_020623.RDS')
+  # # se <- readRDS('se_020623.RDS')
   
   
   # Estimate Size Factors (to account for differences in sequencing depth)
@@ -90,7 +90,8 @@
   
   
   # Results
-  Contrast <- c('condition','IMR_100J_UVC','IMR_200J_UVB')
+  #Contrast <- c('condition','IMR_100J_UVC','IMR_200J_UVB')
+  Contrast <- c('condition','TP53_200J_UVB','IMR_200J_UVB')
   res <- results(dds,contrast=Contrast)
   resASH <- lfcShrink(dds, contrast=Contrast, type='ashr') # log fold-change shrinkage for visualizations
   resASH$lambda_control <- controls
@@ -146,7 +147,9 @@
     theme_bw()
   pv2 <- ggplot(as.data.frame(resASH),aes(x=log2FoldChange,y=minus_log10_pval)) +
     geom_point(aes(color=DSR)) +
+    #geom_point(aes(fill=lambda_control,color=DSR),pch=21) +
     scale_color_manual(values=c('gray','#FC9272')) +
+    #scale_fill_manual(values=c('gray','dodgerblue')) +
     xlim(c(-4,4)) +
     ylim(c(0,20)) +
     ylab('-log10(pvalue)') +
@@ -157,12 +160,32 @@
   annotate_figure(DSR_plots,top=paste(Contrast[2],'vs',Contrast[3]))
   
   
-  
-  
-  
-  
-  
-  
+  # MA and Volcano plots option 2
+  pm1 <- ggplot(as.data.frame(resASH),aes(x=baseMean,y=log2FoldChange)) +
+    geom_point(aes(fill=lambda_control,color=DSR),pch=21) +
+    scale_color_manual(values=c('gray','#FC9272')) +
+    scale_fill_manual(values=c('gray','dodgerblue')) +
+    geom_hline(yintercept=c(-1,1),linetype='dashed') +
+    scale_x_continuous(trans='log10',limits=c(0.01,15000)) +
+    ylim(c(-4,4)) +
+    labs(color='lambda control') +
+    xlab('mean susceptibility') +
+    ylab('log fold-change') +
+    guides(color='none') +
+    theme_bw()
+  pv2 <- ggplot(as.data.frame(resASH),aes(x=log2FoldChange,y=minus_log10_pval)) +
+    #geom_point(aes(color=DSR)) +
+    geom_point(aes(fill=lambda_control,color=DSR),pch=21) +
+    scale_color_manual(values=c('gray','#FC9272')) +
+    scale_fill_manual(values=c('gray','dodgerblue')) +
+    xlim(c(-4,4)) +
+    ylim(c(0,20)) +
+    ylab('-log10(pvalue)') +
+    geom_hline(yintercept = -log10(DSR_thresh[1]), linetype='dashed') +
+    geom_vline(xintercept = c(-1*DSR_thresh[2],DSR_thresh[2]), linetype='dashed') + 
+    theme_bw()
+  DSR_plots <- ggarrange(pm1,pv2,ncol=2,widths=c(1,1.5))
+  annotate_figure(DSR_plots,top=paste(Contrast[2],'vs',Contrast[3]))
   
   
   # PCA plots
